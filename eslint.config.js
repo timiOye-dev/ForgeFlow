5662761b92bca4b26cd8760c4098e167cb2b2fbe
timiOye-dev <timioye84@gmail.com>
2026-08-22 20:58:58 +0100

    configure prettier and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -24,4 +25,5 @@ export default defineConfig([
       ],
     },
   },
+  eslintConfigPrettier,
 ])
